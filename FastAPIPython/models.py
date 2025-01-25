ca00d0580644f83aed62a760878d7064452f1634
juliusya45 <derpyjul45@gmail.com>
2025-01-25 12:10:54 -0800

    Moved to using Motor Async, gives JSON data of random property

--- FastAPIPython/models.py
+++ FastAPIPython/models.py
@@ -5,13 +5,39 @@ from pydantic import BaseModel, Field
 class Property(BaseModel):
     MAK: int = Field(...)
     PropertyAddress: str = Field(...)
+    City: str = Field(...)
+    State: str = Field(...)
+    ZipCode: int = Field(...)
+    CountyLandUseDescription: str = Field(...)
+    TotalAssessedValue: int = Field(...)
+    AssessmentYear: int = Field(...)
+    LotSizeOrArea: int = Field(...)
+    YearBuilt: int = Field(...)
+    TotalNumberOfRooms: int = Field(...)
+    NumberOfBedrooms: int = Field(...)
+    NumberOfBaths: int = Field(...)
+    NumberOfStories: int = Field(...)
+    PoolArea: int = Field(...)
 
     class Config:
         allow_population_by_field_name = True
         schema_extra = {
             "example": {
                 "MAK": 4945331197,
-                "PropertyAddress": "1806 W 19th St"
+                "PropertyAddress": "1806 W 19th St",
+                "City": "Santa Ana",
+                "State": "CA",
+                "ZipCode": 92706,
+                "CountyLandUseDescription": "SINGLE FAMILY RESIDENTIAL",
+                "TotalAssessedValue": 280428,
+                "AssessmentYear": 2023,
+                "LotSizeOrArea": 6050,
+                "YearBuilt": 1951,
+                "TotalNumberOfRooms": 4,
+                "NumberOfBedrooms": 2,
+                "NumberOfBaths": 1,
+                "NumberOfStories": 1,
+                "PoolArea": 0,
             }
         }
 
