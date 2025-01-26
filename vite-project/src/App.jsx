137642730b5ed78c6395247df1065e05acc76840
Sean Liem <syliem@uci.edu>
2025-01-25 16:33:13 -0800

    Zoom and Lon/Lat Figured out

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,6 +5,10 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 import './App.css'
 
+const INITIAL_CENTER = [-117.8389, 33.6405]
+
+const INITIAL_ZOOM = 17
+
 function App() {
   console.log('Hello');
   const apiKey = import.meta.env.VITE_MAP_KEY;
@@ -13,10 +17,15 @@ function App() {
   const mapRef = useRef()
   const mapContainerRef = useRef()
 
+  //const [center, setCenter] = useState(INITIAL_CENTER)
+  //const [zoom, setZoom] = useState(INITIAL_ZOOM)
+
   useEffect(() => {
     mapboxgl.accessToken = apiKey
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
+      center: INITIAL_CENTER,
+      zoom: INITIAL_ZOOM
     });
 
     return () => {
